d56f644671d9c83aae24b18593c72e21694bd502
Nathan Arthur <narthur-git@rainskit.com>
2025-12-05 20:24:59 -0500

    Day 5 part 2

--- 05/src/main.rs
+++ 05/src/main.rs
@@ -23,6 +23,7 @@ fn main() {
     process(contents);
 }
 
+#[derive(Clone, Debug)]
 struct Range {
     min: u64,
     max: u64,
@@ -36,26 +37,67 @@ impl Range {
 
 fn process(data: String) {
     let mut ranges: Vec<Range> = vec![];
-    let mut fresh = 0;
 
-    let mut range_mode = true;
     for line in data.lines() {
-        if range_mode {
-            if line == "" {
-                range_mode = false;
-            } else {
-                let (min, max) = line.split_once("-").unwrap();
-                ranges.push(Range {
-                    min: min.parse().unwrap(),
-                    max: max.parse().unwrap(),
-                });
-            }
+        if line == "" {
+            break;
         } else {
-            let val = line.parse().unwrap();
-            if ranges.iter().any(|range| range.includes(val)) {
-                fresh += 1;
-            }
+            let (min, max) = line.split_once("-").unwrap();
+            ranges.push(Range {
+                min: min.parse().unwrap(),
+                max: max.parse().unwrap(),
+            });
         }
     }
+
+    loop {
+        let (new_ranges, merge_count) = merge_ranges(&ranges);
+        ranges = new_ranges;
+        if merge_count == 0 {
+            break;
+        }
+    }
+
+    let fresh: u64 = ranges.iter().map(|range| range.max - range.min + 1).sum();
     println!("{fresh}");
+}
+
+fn merge_ranges(ranges: &Vec<Range>) -> (Vec<Range>, usize) {
+    let mut merged: Vec<Range> = vec![];
+    let mut merged_count = 0;
+    'range_loop: for range in ranges.iter() {
+        let merged_len = merged.len();
+        for m in 0..merged_len {
+            let merge = &merged[m];
+            if merge.includes(range.min) {
+                if merge.includes(range.max) {
+                    // do nothing; this range is already covered
+                    merged_count += 1;
+                    continue 'range_loop;
+                } else {
+                    // merge range and merge
+                    merged.push(Range {
+                        min: merge.min,
+                        max: range.max,
+                    });
+                    merged.swap_remove(m);
+                    merged_count += 1;
+                    continue 'range_loop;
+                }
+            } else if merge.includes(range.max) {
+                // merge range and merge
+                merged.push(Range {
+                    min: range.min,
+                    max: merge.max,
+                });
+                merged.swap_remove(m);
+                merged_count += 1;
+                continue 'range_loop;
+            }
+        }
+        // if we got here, this range is distinct
+        merged.push((*range).clone());
+    }
+    merged.sort_by(|a, b| a.min.cmp(&b.min));
+    (merged, merged_count)
 }
